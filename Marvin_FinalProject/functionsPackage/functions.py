ad0e4bb42accf0c1504acb40646f20a82dfd40ea
harrints <harrints@vdi-LCB-IC-006.ad.uc.edu>
2024-04-17 11:03:34 -0400

    I added Documentation to the projects as well as a new function package and project to increase the modularity of the project

---
+++ Marvin_FinalProject/functionsPackage/functions.py
@@ -0,0 +1,30 @@
+#functions.py
+
+# Name: Ben Klein, TJ Harrington, Ryan Wilkins
+# email: kleinbw@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date: 4/23/24
+# Course/Section: IS 4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: In the final project, we are in a group working together to decipher a code that will reveal a location on campus for us to find
+# Brief Description of what this module does. The Functions module contains all of the functions we wrote for the final project. 
+# Citations: 
+from decryptionPackage.decryption import *
+from txtPackage.txt import *
+
+
+def decrypt_Marvin(data, words):
+    decrypted_values = []
+    for x in data:
+        y = int(x)
+        decrypted_values.append(words[y])
+    return decrypted_values
+
+Data = readJSON("EncryptedGroupHints Spring 2024 Section 002.json")
+Marvin = Data["Marvin"]
+print(Marvin) #Can be deleted
+#Creating a DF For the words 
+words = read_words_from_file("UCEnglish.txt")
+
+decrypted_values = decrypt_Marvin(Marvin, words)
+print(decrypted_values)
