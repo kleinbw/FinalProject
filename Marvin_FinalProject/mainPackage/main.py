46bdfcd9a8cd297de1d03af3f111ff857ce5ae2f
rRWilkins <157031289+rRWilkins@users.noreply.github.com>
2024-04-18 09:10:15 -0400

    Update main.py

--- Marvin_FinalProject/mainPackage/main.py
+++ Marvin_FinalProject/mainPackage/main.py
@@ -6,25 +6,14 @@
 # Semester/Year: Spring 2024
 # Brief Description of the assignment: In the final project, we are in a group working together to decipher a code that will reveal a location on campus for us to find
 # Brief Description of what this module does. This is the main module and in here all of our code runs. this is where we determine what the decryption is 
-# Citations: 
+# Citations: None
 
 from decryptionPackage.decryption import *
 from functionsPackage.functions import *
 from txtPackage.txt import *
+from PIL import Image
 
 if __name__ == "__main__":
-
-
-    Data = readJSON("EncryptedGroupHints Spring 2024 Section 002.json")
-    Marvin = Data["Marvin"]
-    #print(Marvin) #Can be deleted
-    #Creating a DF For the words 
-    words = read_words_from_file("UCEnglish.txt")
-    
-    #decrypted_values = decrypt_Marvin(Marvin, words)
-    #print(decrypted_values)
-    
-    print(decrypt_Marvin(Marvin, words))
 
     #EXTRACTING RAW ENCRYPTION DATA
     Encrypted = readJSON("TeamsAndEncryptedMessagesForDistribution - 002.json")
@@ -32,3 +21,5 @@ if __name__ == "__main__":
     Encrypted = str(Encrypted)
     
     print(decryption(Encrypted))   
+    myimage = load_image(("FinalPhoto.jpeg"))
+    myimage.show()
