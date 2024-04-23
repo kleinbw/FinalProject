65a35a5c1bfc35cb014fc011393bc975dfadd059
tjharr20 <157031387+tjharr20@users.noreply.github.com>
2024-04-22 20:24:46 -0400

    Update main.py

--- Marvin_FinalProject/mainPackage/main.py
+++ Marvin_FinalProject/mainPackage/main.py
@@ -8,22 +8,26 @@
 # Brief Description of what this module does. This is the main module and in here all of our code runs. this is where we determine what the decryption is 
 # Citations: None
 
+
 from decryptionPackage.decryption import *
-from functionsPackage.functions import *
+from functionsPackage.functions import decrypt_Marvin
+from functionsPackage.functions import load_image
 from txtPackage.txt import *
 from PIL import Image
 
 if __name__ == "__main__":
-    Data = readJSON("EncryptedGroupHints Spring 2024 Section 002.json")
-    Marvin = Data["Marvin"]
-    words = read_words_from_file("UCEnglish.txt")
-    print(decrypt_Marvin(Marvin, words))
 
-    #EXTRACTING RAW ENCRYPTION DATA
-    Encrypted = readJSON("TeamsAndEncryptedMessagesForDistribution - 002.json")
-    Encrypted = Encrypted["Marvin"]
-    Encrypted = str(Encrypted)
-    
-    print(decryption(Encrypted))   
-    myimage = load_image(("FinalPhoto.jpeg"))
-    myimage.show()
+   Data = readJSON("EncryptedGroupHints Spring 2024 Section 002.json")
+   Marvin = Data["Marvin"]
+   words = read_words_from_file("UCEnglish.txt")
+   print(decrypt_Marvin(Marvin, words))
+
+   #EXTRACTING RAW ENCRYPTION DATA
+
+   Encrypted = readJSON("TeamsAndEncryptedMessagesForDistribution - 002.json")
+   Encrypted = Encrypted["Marvin"]
+   Encrypted = str(Encrypted)
+
+   print(decryption(Encrypted))   
+   myimage = load_image("FinalPhoto.jpeg")
+   myimage.show()
